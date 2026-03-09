5ac14bf2fdbb6a09d28cad2885cb359fa676b35f
Carlos Hernandez <charlesshb98@gmail.com>
2026-03-09 11:56:57 -0400

    feat: add Spacer and Title components with styling, and integrate nanoid dependency

---
+++ src/Page/Title/Title.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useRef } from "react";
+import type { NodeData } from "../../utils/types";
+import styles from "./Title.module.css";
+import { nanoid } from "nanoid";
+
+type TitleProps = {
+    title: string;
+    changePageTitle(title: string): void;
+    addNode(node: NodeData, index: number): void;
+}
+
+export const Title = ({ title, changePageTitle, addNode }: TitleProps) => {
+    const headerRef = useRef<HTMLHeadingElement>(null);
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLHeadingElement>) => {
+        if (e.key === "Enter") {
+            e.preventDefault();
+            addNode({ type: "text", id: nanoid(), value: "" }, 0);
+        }
+    };
+
+    const handleInputChange = (e: React.InputEvent<HTMLHeadingElement>) => {
+        changePageTitle(e.currentTarget.textContent || "");
+    };
+
+    useEffect(() => {
+        const isFocused = document.activeElement === headerRef.current;
+        if (!isFocused && headerRef.current) {
+            headerRef.current.textContent = title;
+        }
+    }, [title]);
+
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title} 
+                ref={headerRef}
+                contentEditable
+                suppressContentEditableWarning
+                onKeyDown={handleKeyDown}
+                onInput={handleInputChange}
+                />
+        </div>
+    );
+}
